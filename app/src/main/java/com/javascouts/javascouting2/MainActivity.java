e5fbd807660b8fe3dce0c093356e846bc8044e00
Linus\Leon <javascouts@gmail.com>
2019-01-17 20:03:01 -0500

    Add initial fragments, basic layouts.

--- app/src/main/java/com/javascouts/javascouting2/MainActivity.java
+++ app/src/main/java/com/javascouts/javascouting2/MainActivity.java
@@ -1,21 +1,39 @@
 package com.javascouts.javascouting2;
 
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String current;
+    Fragment scoutingFragment, scheduleFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        scoutingFragment = new ScoutingFragment();
+        scheduleFragment = new ScheduleFragment();
+
+        fragmentTransaction.replace(R.id.fragment_main, scoutingFragment);
+
+        fragmentTransaction.commit();
+        current = "SCOUTING";
+
 
     }
 
@@ -25,13 +43,27 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_scouting:
+                    if(current.equals("SCOUTING")){
+                        return true;
+                    }
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_main, scoutingFragment);
+                    transaction.commit();
+                    current = "SCOUTING";
 
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_schedule:
+                    if(current.equals("SCHEDULE")){
+                        return true;
+                    }
+                    FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                    transaction2.replace(R.id.fragment_main, scheduleFragment);
+                    transaction2.commit();
+                    current = "SCHEDULE";
 
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_analysis:
 
                     return true;
             }
---
+++ app/src/main/java/com/javascouts/javascouting2/ScheduleFragment.java
@@ -0,0 +1,22 @@
+package com.javascouts.javascouting2;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Liam on 1/17/2019.
+ */
+
+public class ScheduleFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+}
